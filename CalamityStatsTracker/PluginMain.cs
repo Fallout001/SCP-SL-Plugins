25499e0e0c31c17363f06b38895deedc964053a6
Fallout001 <fallingout081@gmail.com>
2025-06-06 11:05:28 -0400

    fix project files and make stats tracker working to some extent

--- CalamityStatsTracker/PluginMain.cs
+++ CalamityStatsTracker/PluginMain.cs
@@ -4,14 +4,16 @@ using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.Arguments.ServerEvents;
 using LabApi.Events.Handlers;
 using LabApi.Loader.Features.Plugins;
+using LabApi;
 using MEC;
 using System.Xml.Serialization;
+using EffectOnHUD;
 
 namespace CalamityStatsTracker
 {
     [CommandHandler(typeof(ClientCommandHandler))]
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
-    public class CalamityStatsTracker : Plugin<PluginConfig>
+    public class CalamityStatsTracker : LabApi.Loader.Features.Plugins.Plugin<PluginConfig>
     {
         public override string Author => "Fallout001";
         public override string Name => "CalamityStatsTracker";
@@ -24,26 +26,44 @@ namespace CalamityStatsTracker
         public override void Enable()
         {
             Instance = this;
-            ServerEvents.RoundStarted += OnRoundStarted;
-            //sub round ending
+            ServerEvents.RoundStarted += OnRoundStarted; 
+            ServerEvents.RoundEnding += OnRoundEnding; 
             CL.Info("Calamity Stats Tracker Enabled");
         }
 
         public override void Disable()
         {
             Instance = null;
-            ServerEvents.RoundStarted -= OnRoundStarted; // unsub round start
-            //unsub round end
+            ServerEvents.RoundStarted -= OnRoundStarted; 
+            ServerEvents.RoundEnding -= OnRoundEnding;
             CL.Info("Calamity Stats Tracker Disabled");
         }
 
-        void OnRoundStarted(RoundStartingEventArgs ev) // make on round start arg
+        void OnRoundStarted()
         {
             //clear previous stats from last round or etc
+            CL.Error("Round Started ARG");
+            RoundStatsTracker.AddStatEvent("CalamityStatsTracker", "Round", "RoundStarted","Round has started");
+            RoundStatsTracker.AddStatEvent("CalamityStatsTracker", "Round", "RoundStarted","Round has started");
         }
 
-        //make on round end arg
-        //call the saving of stats function
+        void OnRoundEnding(RoundEndingEventArgs ev) // Added handler for round ending
+        {
+            CL.Error("Round Ended  ARG");
+            var roundEndEvent = new BaseStatEvent
+            {
+                PluginName = "CalamityStatsTracker",
+                EventType = "Round",
+                EventName = "RoundEnded",
+                Timestamp = DateTime.Now,
+                RoundTime = DateTime.Now.Second - RoundStatsTracker.roundStartTime, // Assuming RoundDuration is in seconds
+                ExtraData = $"Winning Team: {ev.LeadingTeam}"
+            };
+            CL.Error("Round Ending and attempt add");
+            RoundStatsTracker.CurrentRoundStats.Add(roundEndEvent);
+
+            RoundStatsTracker.SaveCurrentRoundStats(); // Save stats when round ends
+        }
     }
 }
 
--- CalamityStatsTracker/RoundStatsTracker.cs
+++ CalamityStatsTracker/RoundStatsTracker.cs
@@ -4,20 +4,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LabApi;
+using System.IO;
 
 namespace EffectOnHUD
 {
     internal class RoundStatsTracker
     {
         public static List<BaseStatEvent> CurrentRoundStats = new();
-        public static void SaveCurrentRoundStats(int roundNumber)
+
+        public static int roundStartTime = DateTime.Now.Second;
+
+        public static void SaveCurrentRoundStats()
         {
-           foreach (var statEvent in CurrentRoundStats)
+            foreach (var statEvent in CurrentRoundStats)
             {
-                
+                string statsDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "StatsTracker");
+                Directory.CreateDirectory(statsDir);
+
+                string fileName = $"RoundStats_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+                string filePath = Path.Combine(statsDir, fileName);
+
+                var line = CurrentRoundStats.Select(statEvent =>
+                $"{statEvent.Timestamp:u} | {statEvent.PluginName} | {statEvent.EventType} | {statEvent.EventName} | {statEvent.RoundTime}s | {statEvent.ExtraData}");
+
+                CL.Error($"Saving stats to {filePath}");
+
+                File.WriteAllLines(filePath, line);
             }
-            // Clear the stats after saving jusst in case 
+            // Clear the stats after saving just in case 
             CurrentRoundStats.Clear();
+        }
+
+        public static void AddStatEvent(string PluginName, string EventType, string EventName, string ExtraData)
+        {
+            var StatEvent = new BaseStatEvent
+            {
+                PluginName = PluginName,
+                EventType = EventType,
+                EventName = EventName,
+                Timestamp = DateTime.Now,
+                RoundTime = DateTime.Now.Second - roundStartTime,
+                ExtraData = ExtraData
+            };
+            
+            CL.Error($"Adding stat event: {StatEvent.EventName} at {StatEvent.Timestamp:u}");
+            CurrentRoundStats.Add(StatEvent);
+
         }
     }
 }
+    
+     
